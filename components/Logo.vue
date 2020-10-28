51899bc22727d1686f75ccd34984d506848f9cf4
spdbear <16793732+spdbear@users.noreply.github.com>
2020-10-28 15:32:46 +0900

    背景画像の挿入も対応

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,62 +1,86 @@
 <template>
-  <svg
-    class="NuxtLogo"
-    width="960"
-    height="540"
-    viewBox="0 0 960 540"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <text
-      class="background2 text"
-      :x="pos.x"
-      :y="pos.y"
-      :font-size="size"
-      :stroke="color2"
-      :stroke-width="stroke1 + stroke2"
-    >
-      {{ text }}
-    </text>
-    <text
-      class="background text"
-      :x="pos.x"
-      :y="pos.y"
-      :font-size="size"
-      :stroke="color1"
-      :stroke-width="stroke2"
-    >
-      {{ text }}
-    </text>
-    <text
-      class="foreground text"
-      :x="pos.x"
-      :y="pos.y"
-      :font-size="size"
-      :stroke="color2"
-      :fill="color1"
-      :stroke-width="stroke1"
-    >
-      {{ text }}
-    </text>
-    <rect
-      x="0"
-      y="0"
+  <div>
+    <svg
+      class="NuxtLogo"
       width="960"
       height="540"
-      fill="transparent"
-      stroke="black"
-    />
-  </svg>
+      viewBox="0 0 960 540"
+      xmlns="http://www.w3.org/2000/svg"
+      @mousemove="touchMove($event)"
+      @mouseup="touchEnd($event)"
+      @mouseleave="touchEnd($event)"
+    >
+      <image :href="url" width="960" height="540" />
+      <rect
+        x="0"
+        y="0"
+        width="960"
+        height="540"
+        fill="transparent"
+        stroke="black"
+      />
+      <text
+        class="background2 text"
+        :x="pos.x"
+        :y="pos.y"
+        :font-size="size"
+        :stroke="color2"
+        :stroke-width="stroke1 + stroke2"
+        @mousedown="touchStart($event)"
+      >
+        {{ text }}
+      </text>
+      <text
+        class="background text"
+        :x="pos.x"
+        :y="pos.y"
+        :font-size="size"
+        :stroke="color1"
+        :stroke-width="stroke2"
+        @mousedown="touchStart($event)"
+      >
+        {{ text }}
+      </text>
+      <text
+        class="foreground text"
+        :x="pos.x"
+        :y="pos.y"
+        :font-size="size"
+        :stroke="color2"
+        :fill="color1"
+        :stroke-width="stroke1"
+        @mousedown="touchStart($event)"
+      >
+        {{ text }}
+      </text>
+    </svg>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref, reactive, computed } from '@vue/composition-api'
 
 type Props = {
   text: string
+  color1: string
+  color2: string
+  url: string
 }
 
 export default defineComponent({
   props: {
     text: {
+      type: String,
+      default: '',
+    },
+    color1: {
+      type: String,
+      default: '#ffffff',
+    },
+    color2: {
+      type: String,
+      default: '#000000',
+    },
+    url: {
       type: String,
       default: '',
     },
@@ -67,11 +91,43 @@ export default defineComponent({
       y: 100,
     })
     const size = ref(80)
-    const color1 = ref('rgb(255,255,255)')
-    const color2 = ref('rgb(0,0,0)')
     const stroke1 = ref(3)
     const stroke2 = ref(10)
     const text = computed(() => props.text)
+    const color1 = computed(() => props.color1)
+    const color2 = computed(() => props.color2)
+    const isMouseDown = ref(false)
+    const prevPos = reactive({
+      x: pos.x,
+      y: pos.y,
+    })
+
+    const touchStart = (e: MouseEvent) => {
+      isMouseDown.value = true
+      console.log('touch start:%d,%d', e.offsetX, e.offsetY)
+      prevPos.x = e.offsetX
+      prevPos.y = e.offsetY
+    }
+    const touchMove = (e: MouseEvent) => {
+      // 押下中だったら
+      if (isMouseDown.value) {
+        // 前回座標との差分を算出
+        const movedX = e.offsetX - prevPos.x
+        const movedY = e.offsetY - prevPos.y
+
+        pos.x += movedX
+        pos.y += movedY
+
+        // 前回のクリック座標を更新
+        prevPos.x = e.offsetX
+        prevPos.y = e.offsetY
+      }
+    }
+    const touchEnd = () => {
+      isMouseDown.value = false
+      console.log('touch end')
+    }
+
     return {
       color1,
       color2,
@@ -80,6 +136,9 @@ export default defineComponent({
       text,
       pos,
       size,
+      touchStart,
+      touchMove,
+      touchEnd,
     }
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="container">
+    <h1 class="title">telop-generator</h1>
     <div>
-      <h1 class="title">telop-generator</h1>
-      <Logo :text="text" />
+      <Logo :text="text" :color1="color1" :color2="color2" :url="url" />
     </div>
-    <input v-model="text" placeholder="YouTubeっぽい字幕" />
+    <div>
+      <input v-model="text" placeholder="ここにテキストを入力" />
+      <input v-model="color1" type="color" value="#ffffff" />色1
+      <input v-model="color2" type="color" value="#000000" />色2
+      <input
+        type="file"
+        accept="image/jpg,image/png,image/gif"
+        @change="handleInputImage"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Logo from '~/components/Logo.vue'
+type HTMLInputEvent = Event & {
+  target: HTMLInputElement & EventTarget
+}
 
 export default Vue.extend({
   components: {
@@ -19,7 +31,19 @@ export default Vue.extend({
   data() {
     return {
       text: '',
+      color1: '#ffffff',
+      color2: '#000000',
+      url: '',
     }
+  },
+  methods: {
+    handleInputImage(e: HTMLInputEvent) {
+      if (!e.target.files?.length) {
+        return
+      }
+      const file = e.target.files[0]
+      this.url = window.URL.createObjectURL(file)
+    },
   },
 })
 </script>
@@ -30,29 +54,17 @@ export default Vue.extend({
   min-height: 100vh;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   text-align: center;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'けいふぉんと', sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
+  font-weight: 600;
+  font-size: 80px;
+  margin: 10px;
   color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
